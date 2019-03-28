67a0026c140820bb7717342c770e99302d5de068
D7703_19 <d7703_19@d7703-19ui-imac.local>
2019-03-28 12:46:33 +0900

    aa

--- stackview/ViewController.swift
+++ stackview/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgaeview: UIImageView!
+    var count = 1
+    var myTimer = Timer() //타이머객체생성
+    var isAnimating = false
+    @IBOutlet weak var lbla: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgaeview.image = UIImage(named: "Image-\(count)")
+        lbla.text = String(count)
     }
 
+    @IBAction func btnplay(_ sender: Any) {
+        print("play")
+        
+        if isAnimating == true { return }
+        else {isAnimating = true}
+        myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+            isAnimating = true
+        
+    }
+    
+    @IBAction func btnpause(_ sender: Any) {
 
+        myTimer.invalidate()
+        
+        isAnimating = false
+    }
+    
+    @IBAction func btnStop(_ sender: Any) {
+        print("btnStop")
+        myTimer.invalidate()
+        count = 0
+    }
+    @objc func doAnimation(){
+        if count == 5{
+            count = 0
+        }
+        count += 1
+        
+        imgaeview.image = UIImage(named: "Image-\(count)")
+        lbla.text = String(count)
+    }
 }
 
